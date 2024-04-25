aba6f5efc0e028669ec091a26653680953d676b1
Jesus Sunuc <mariano.code4guate@gmail.com>
2024-04-25 11:20:58 -0600

    update2.0

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25041fa64b98d6d1903beaab0db4c059148c3a9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f37ed0900f34c53a29f0257eb92a8bdf6180d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
